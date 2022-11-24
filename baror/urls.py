20b20203cb43ad22e001c6d72f49dc6b2083ae6a
Amit Hauzer <amit.hauzer@gmail.com>
2022-11-24 19:14:42 +0200

    add soldiers to barors list, add statuses

--- baror/urls.py
+++ baror/urls.py
@@ -4,9 +4,15 @@ from . import views
 app_name = 'barors'
 urlpatterns = [
     path('', views.list_of_barors, name='all-barors'),
-    path('createbaror/', views.create_baror, name='create-baror'),
     path('addround/', views.add_baror_round, name='add-baror'),
     path('<pk>/editround/', views.edit_baror, name='edit-baror'),
     path('addsoldier/', views.add_soldier_to_round, name='add-soldier-to-round'),
-
+    path('<pk>/barorReady/', views.baror_is_ready, name='baror-is-ready'),
 ]
+
+
+
+
+
+# Old:
+# path('createbaror/', views.create_baror, name='create-baror'),
--- soldiers/models.py
+++ soldiers/models.py
@@ -2,9 +2,15 @@ from django.db import models
 
 # Create your models here.
 class Soldier(models.Model):
-   
+    Soldier_SATUTS = {
+        'Waiting for Baror':'Waiting for Baror',
+        'Ready to run':'Ready to run',
+        'Running':'Running',
+        'Done':'Done', 
+    }
+
     name = models.CharField(max_length=50)
-    soldier_status = models.CharField(max_length=50 , default='Waiting for Baror')
+    soldier_status = models.CharField(max_length=50 , default=f'{Soldier_SATUTS["Waiting for Baror"]}')
 
     def __str__(self) -> str:
         return f'{self.name}'
