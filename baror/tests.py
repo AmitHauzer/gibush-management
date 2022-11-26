724a5392e79b96241ec32cbfe141b6eba195a722
Amit Hauzer <amit.hauzer@gmail.com>
2022-11-27 00:43:21 +0200

    only test

--- baror/tests.py
+++ baror/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from datetime import datetime
+
+
 
 # Create your tests here.
+def start1():
+    start = datetime.now()
+    return start
+
+
+def now1(start):
+    now = datetime.now()
+    now.strftime("%H:%M:%S:%f")[:-4]
+    print(now-start)
+
+
+
