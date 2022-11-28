d4b563014ace06a2ee9411723be978806eca559f
Amit Hauzer <amit.hauzer@gmail.com>
2022-11-28 19:29:03 +0200

    add shalishut

--- shalishut/views.py
+++ shalishut/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from soldiers.models import Soldier
 
 # Create your views here.
+def menu(request):
+    soldiers = Soldier.objects.all()
+    return render(request, 'shalishut_menu.html',{'soldiers':soldiers})
