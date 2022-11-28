c4a1b69aa3bc9687994665b66287cad55617ab1e
Amit Hauzer <amit.hauzer@gmail.com>
2022-11-28 12:41:01 +0200

    init home page

--- gibush/urls.py
+++ gibush/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from soldiers import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home_page),
     path('soldiers/', include('soldiers.urls')),
     path('barors/', include('baror.urls'))
 ]
