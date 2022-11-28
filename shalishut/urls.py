a3bf2c74896c070a3b9c20313bc1868eaa2e3335
Amit Hauzer <amit.hauzer@gmail.com>
2022-11-28 20:21:48 +0200

    add soldier

--- shalishut/urls.py
+++ shalishut/urls.py
@@ -4,4 +4,6 @@ from . import views
 app_name = 'shalishut'
 urlpatterns = [
     path('', views.menu, name='menu-shalishut'),
+    path('addsoldier/', views.add_soldier, name='add-soldier-shalishut'),
+    path('<pk>/updatesoldier', views.add_soldier, name='update-soldier-shalishut'),
 ]
