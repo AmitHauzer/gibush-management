75888acfda09ca86c00d5606dbbb7c270def53bc
Amit Hauzer <amit.hauzer@gmail.com>
2022-12-02 00:40:54 +0200

    static settings

--- gibush/settings.py
+++ gibush/settings.py
@@ -120,7 +120,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+MEDIA_URL = '/images/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    # '/var/www/static/',
+]
+
+MEDIA_ROOT = BASE_DIR / 'static/soldiers_docs'
+
+STATIC_ROOT =  BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
