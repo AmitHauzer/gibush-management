8efa4dcc8298ae2e9e18d4fa14f951a61afd7a5e
Amit Hauzer <amit.hauzer@gmail.com>
2022-12-01 20:53:20 +0200

    add clinic menu

--- clinic/views.py
+++ clinic/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from soldiers.models import Soldier
+
 
 # Create your views here.
 def menu(request):
-    return HttpResponse('clinic test')
+    soldiers_befor = Soldier.objects.filter(soldier_status=Soldier.SoldierStatus.WAITING_FOR_SHALISHUT)
+    soldiers_after = Soldier.objects.exclude(soldier_status=Soldier.SoldierStatus.WAITING_FOR_SHALISHUT)
+    return render(request, 'clinic_menu.html',{'soldiers':{'before':soldiers_befor, 'after':soldiers_after}})
