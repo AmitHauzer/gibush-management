8b5740132b66d93f4928745a6c9d7d4bf03bb057
Amit Hauzer <amit.hauzer@gmail.com>
2022-12-03 21:48:59 +0200

    add 'search' and 'update soldier'

--- clinic/urls.py
+++ clinic/urls.py
@@ -4,7 +4,8 @@ from . import views
 app_name = 'clinic'
 urlpatterns = [
     path('', views.menu, name='menu-clinic'),
+    path('search/', views.search, name='search-clinic'),
     # path('addsoldier/', views.add_soldier, name='add-soldier-shalishut'),
-    # path('<pk>/updatesoldier', views.update_soldier, name='update-soldier-shalishut'),
+    path('<pk>/updatesoldier', views.update_soldier, name='update-soldier-clinic'),
     
 ]
