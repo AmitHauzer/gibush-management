256563c35609f064ffba61cee4537cc15cb610e8
Amit Hauzer <amit.hauzer@gmail.com>
2022-12-05 20:31:29 +0200

    menu

--- commander/views.py
+++ commander/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from soldiers.models import Soldier
 
-# Create your views here.
+
 def menu(request):
-    return HttpResponse('commander test')
+    # A dictionary which ordered by all the statuses
+    status = {}
+    for s in Soldier.SoldierStatus:
+        key = str(s).upper().replace(' ','_')
+        status[key]=Soldier.objects.filter(soldier_status=s)
+    return render(request,'commander_menu.html', {'status':status})
