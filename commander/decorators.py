8d8bc4fb5a9f3624cd2a001d798dbe2b3f5e0be6
Amit Hauzer <amit.hauzer@gmail.com>
2022-12-23 13:35:31 +0200

    add login required in allowed users func.

--- commander/decorators.py
+++ commander/decorators.py
@@ -4,7 +4,7 @@ from urllib.parse import urlparse
 from django.conf import settings
 from django.contrib.auth import REDIRECT_FIELD_NAME
 from django.core.exceptions import PermissionDenied
-from django.shortcuts import resolve_url
+from django.shortcuts import redirect, resolve_url
 
 
 def user_passes_test(
@@ -55,12 +55,26 @@ def login_required(
         login_url=login_url,
         redirect_field_name=redirect_field_name,
     )
-    # if actual_decorator == False:
-        #  messages.error(request, 'ERRORRRRR')
     if function:
         return actual_decorator(function)
     return actual_decorator
 
+
+def allowed_users(allowed_roles=[]):
+    def decorator(view_func):
+        @login_required
+        def wrapper_func(request,*args,**kwargs):
+            
+            group = None
+            if request.user.groups.exists():
+                group = request.user.groups.all()[0].name
+            if group in allowed_roles:
+                return view_func(request,*args,**kwargs)
+            else:
+                messages.error(request, "You are not allowed.")
+                return redirect("home-page")
+        return wrapper_func
+    return decorator
 
 # def permission_required(perm, login_url=None, raise_exception=False):
 #     """
