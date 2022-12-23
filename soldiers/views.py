d605536e5ad27ff3920d4d6fcc6279c7f4ffc205
Amit Hauzer <amit.hauzer@gmail.com>
2022-12-23 03:07:03 +0200

    add login required

--- soldiers/views.py
+++ soldiers/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from soldiers.models import Soldier
 
 
+
 # Create your views here.
 def all_soldiers(request):
     soldiers = Soldier.objects.all()
@@ -11,8 +12,6 @@ def all_soldiers(request):
 def single_soldier(request, pk):
     soldier = Soldier.objects.get(id=pk)
     return render(request, 'single_soldier.html',{'soldier':soldier})
-
-
 
 
 def home_page(request):
