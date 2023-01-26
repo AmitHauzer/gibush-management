bb1f2a8d336bf366ab888e87be10ac0cfb9f3d2e
Amit Hauzer <amit.hauzer@gmail.com>
2023-01-26 14:20:37 +0200

    add https://gibush-management-demo.onrender.com/ to allowed hosts

--- gibush/settings.py
+++ gibush/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'https://gibush-management-demo.onrender.com/']
 
 LOGIN_URL = '/users/login/'
 
